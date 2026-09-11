4154fbe85daa77df4fe570ecdd2ebfbc1b66360a
onik-999 <onikdas.dev@gmail.com>
2026-09-11 12:38:57 +0600

    Add solution for problem 02: Word Pattern

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -29,3 +29,38 @@ var isIsomorphic = function(s, t) {
 
 console.log(isIsomorphic("egg", "add")); 
 
+
+
+// 02. Word Pattern
+
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    const words = s.split(' ');
+    if (pattern.length !== words.length) return false;
+    
+    const charToWord = new Map();
+    const wordToChar = new Map();
+    
+    for (let i = 0; i < pattern.length; i++) {
+        let char = pattern[i];
+        let word = words[i];
+        
+        if (charToWord.has(char) && charToWord.get(char) !== word) {
+            return false;
+        }
+        if (wordToChar.has(word) && wordToChar.get(word) !== char) {
+            return false;
+        }
+        
+        charToWord.set(char, word);
+        wordToChar.set(word, char);
+    }
+    
+    return true;
+};
+
+console.log(wordPattern("abba", "dog cat cat dog")); 
