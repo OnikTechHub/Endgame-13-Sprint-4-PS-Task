cd3ba1ba7b83bd2cf62d007a67c1e3eca8282f47
onik-999 <onikdas.dev@gmail.com>
2026-09-11 12:49:46 +0600

    Add solution for problem 03. Find the Difference

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -64,3 +64,29 @@ var wordPattern = function(pattern, s) {
 };
 
 console.log(wordPattern("abba", "dog cat cat dog")); 
+
+
+
+
+// 03. Find the Difference
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let charCodeSum = 0;
+    for (let i = 0; i < t.length; i++) {
+        charCodeSum += t.charCodeAt(i);
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        charCodeSum -= s.charCodeAt(i);
+    }
+    
+    return String.fromCharCode(charCodeSum);
+};
+
+
+console.log(findTheDifference("abcd", "abcde")); 
